a89ab31c749f7bbc7246a468bed95f18e689c69d
sunn789 <sunn789@gmail.com>
2025-03-09 04:15:10 -0700

    add visitoor counter to program

--- Modicom.Raz/Program.cs
+++ Modicom.Raz/Program.cs
@@ -29,7 +29,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseMiddleware<Modicom.Business.Middleware.VisitorTrackingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseRouting();
